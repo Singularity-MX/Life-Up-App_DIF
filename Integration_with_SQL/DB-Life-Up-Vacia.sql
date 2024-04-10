49c556f40f9a7468d58e874b8bb8b4d78fa1ac54
ReplacedSpace17 <replacedspace17@gmail.com>
2024-04-10 15:09:01 -0600

    Configuracion

--- Integration_with_SQL/DB-Life-Up-Vacia.sql
+++ Integration_with_SQL/DB-Life-Up-Vacia.sql
@@ -1,21 +1,3 @@
--- phpMyAdmin SQL Dump
--- version 5.2.1
--- https://www.phpmyadmin.net/
---
--- Servidor: 127.0.0.1
--- Tiempo de generación: 27-08-2023 a las 17:29:53
--- Versión del servidor: 10.4.28-MariaDB
--- Versión de PHP: 8.0.28
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
 
 --
 -- Base de datos: `life-up-db`
@@ -26,26 +8,26 @@ SET time_zone = "+00:00";
 --
 -- Estructura de tabla para la tabla `asistencia`
 --
+CREATE DATABASE Life_Up_DB;
+USE Life_Up_DB;
 
-CREATE TABLE `asistencia` (
-  `TallerID` varchar(20) NOT NULL,
-  `UserID` varchar(20) NOT NULL,
-  `Fecha` date NOT NULL
+-- Nueva Estructura de DB--------------------------------------------------------
+CREATE TABLE `Users` (
+  `UserID` varchar(100) NOT NULL,
+  `Email` varchar(50) NOT NULL,
+  `Password` varchar(100) NOT NULL,
+  `Rol` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- --------------------------------------------------------
-
 --
--- Estructura de tabla para la tabla `centros`
+-- Estructura de tabla para la tabla `InformationPersonal`
 --
-
-CREATE TABLE `centros` (
-  `ID_Centro` varchar(10) NOT NULL,
-  `Nombre` varchar(100) NOT NULL,
-  `Direccion` text NOT NULL,
-  `Delegacion` varchar(100) NOT NULL,
-  `CodigoPostal` int(5) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+CREATE TABLE `InformationPersonal` (
+  `UserID` varchar(100) NOT NULL,
+  `Nombre` varchar(50) NOT NULL,
+  `ApellidoP` varchar(50) NOT NULL,
+  `ApellidoM` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;i;
 
 --
 -- Volcado de datos para la tabla `centros`
