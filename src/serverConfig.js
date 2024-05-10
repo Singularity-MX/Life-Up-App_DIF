d1167f59a285f7ae786b52142eccc65937e35926
ReplacedSpace17 <replacedspace17@gmail.com>
2024-05-10 08:34:41 -0600

    Refactor serverConfig.js to update backend URL to http://localhost:3001

--- src/serverConfig.js
+++ src/serverConfig.js
@@ -1,4 +1,4 @@
 // config.js
-const backendUrl = 'http://localhost:3000'; // Reemplaza esto con la dirección de tu backend
+const backendUrl = 'http://localhost:3001'; // Reemplaza esto con la dirección de tu backend
 
 export default backendUrl;
