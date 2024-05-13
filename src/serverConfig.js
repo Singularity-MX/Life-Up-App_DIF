42b709f11c98a0712bf89108b506ec73c556a584
ReplacedSpace17 <replacedspace17@gmail.com>
2024-05-13 00:26:57 -0600

    Refactorizar serverConfig.js para actualizar la URL del backend a http://20.102.109.114:3001

--- src/serverConfig.js
+++ src/serverConfig.js
@@ -1,4 +1,4 @@
 // config.js
-const backendUrl = 'http://127.0.0.1:3001'; // Reemplaza esto con la dirección de tu backend
+const backendUrl = 'http://20.102.109.114:3001'; // Reemplaza esto con la dirección de tu backend
 //const backendUrl = 'http://20.102.109.114:3001';
 export default backendUrl;
