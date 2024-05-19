e1601ad636800ef6a60009b25414376ea233b97b
ReplacedSpace17 <replacedspace17@gmail.com>
2024-05-19 05:06:41 -0600

    LOGO_GERANTOLOGICAL

--- src/serverConfig.js
+++ src/serverConfig.js
@@ -1,4 +1,5 @@
 // config.js
 //const backendUrl = 'http://127.0.0.1:3001'; // Reemplaza esto con la dirección de tu backend
-const backendUrl = 'http://20.102.109.114:3001';
+//const backendUrl = 'http://20.102.109.114:3001';
+const backendUrl = 'http://localhost:3001';
 export default backendUrl;
